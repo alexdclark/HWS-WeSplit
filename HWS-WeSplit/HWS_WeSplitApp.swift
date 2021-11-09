830c9d537f4e890902c951e5ce250e3b7d550af7
Alex Clark <alexander.d.clark@outlook.com>
2021-11-09 15:48:21 -0600

    Initial Commit

---
+++ HWS-WeSplit/HWS_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_WeSplitApp.swift
+//  HWS-WeSplit
+//
+//  Created by Alexander Clark on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
